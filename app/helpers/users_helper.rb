9c6c227e5d7b634923758b961fd0c0bc9d80a01d
Nick Weiland <nickweiland@gmail.com>
2016-03-24 16:22:52 -0600

    Add table shaming colors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,11 @@
 module UsersHelper
+  def shame(user)
+    if user.updated_at < DateTime.now-3.days
+      return "danger"
+    elsif user.updated_at < DateTime.now-2.days
+      return "warning"
+    else
+      "success"
+    end
+  end
 end
