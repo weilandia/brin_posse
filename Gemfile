63213dd08113a881d325c677b6f42ae780b2ff9b
Nick Weiland <nickweiland@gmail.com>
2016-04-13 18:08:38 -0600

    add exercism.io

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'normalize-rails'
 gem 'bcrypt'
+gem 'faraday'
+gem 'resque', "~> 1.22.0"
 gem 'rails_12factor'
 
 
---
+++ app/models/exercism_service.rb
@@ -0,0 +1,25 @@
+class ExercismService
+  attr_reader :stats
+  def initialize(user)
+    @stats ||= collect_stats(parse(Faraday.get("http://exercism.io/api/v1/users/#{user.github}/statistics")))
+  end
+
+  def collect_stats(response)
+    response["statistics"].select do |track|
+      track["language"] == "Ruby" || track["language"] == "JavaScript"
+    end
+  end
+
+  def ruby
+    @stats.last["completed"].length
+  end
+
+  def js
+    @stats.first["completed"].length
+  end
+
+private
+  def parse(response)
+    JSON.parse(response.body, symbolize_name: true)
+  end
+end
