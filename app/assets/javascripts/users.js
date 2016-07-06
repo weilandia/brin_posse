4ee65bbf314b056d4465582fc833727c45f72808
Nick Weiland <nickweiland@gmail.com>
2016-07-05 23:51:14 -0700

    add js to add success class

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,6 +6,11 @@ $(document).ready(function() {
     $.get("/api/v1/users/" + user + "/exercisms", function ( data ) {
       var $parent = $btn.parent();
 
+      if ($parent.siblings('.total').text() != data.completed_exercism_count) {
+        $parent.parent().addClass("success");
+        $parent.parent().removeClass("danger");
+      }
+
       $parent.siblings('.ruby').text(data.exercism_stats.Ruby);
       $parent.siblings('.js').text(data.exercism_stats.JavaScript);
       $parent.siblings('.total').text(data.completed_exercism_count);
